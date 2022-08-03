ec88d9ed4ca1fe7c7641a592c5c7c1290cd1c87e
crawraps <crawraps@gmail.com>
2022-08-03 15:58:35 +0300

    Add info block shadow and margins

--- src/components/InfoBlock.tsx
+++ src/components/InfoBlock.tsx
@@ -12,7 +12,7 @@ export default function InfoBlock({
   column,
 }: Props): JSX.Element {
   return (
-    <div className='flex flex-col bg-white rounded-md p-4'>
+    <div className='flex flex-col bg-white rounded-md p-4 shadow-md m-2'>
       <h1>{heading}</h1>
       <div className={`flex ${column ? 'flex-col' : ''}`}>{children}</div>
     </div>
